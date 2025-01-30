930170d3cfd1b3446e1fe0fabf0d2c47765f9ddf
adeleeeeyyy <mitsuhaadly@gmail.com>
2025-01-30 21:17:05 +0700

    a

--- commit.sh
+++ commit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 FILE="output.txt"
+COUNT=0  # Inisialisasi counter commit
+
+# Fungsi untuk menangani SIGINT (CTRL+C)
+trap 'echo -e "\nTotal commits: $COUNT"; exit' SIGINT
 
 while true; do
     for char in {a..z} {A..Z} {0..9}; do
@@ -9,6 +13,7 @@ while true; do
         git commit -m "$char"
         git push origin main  # Ganti 'main' jika branch berbeda
         
+        ((COUNT++))  # Tambah counter commit
         sleep 1
     done
 done
